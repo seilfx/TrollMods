1b9a2265eaa62b1b49590b7b33651ab0b678604a
aethercot <99132199+aethercot@users.noreply.github.com>
2022-12-27 15:50:17 +0100

    Initial Commit

---
+++ TrollMods/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TrollMods
+//
+//  Created by Chris on 2022-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrollMods/TrollModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrollModsApp.swift
+//  TrollMods
+//
+//  Created by Chris on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct TrollModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
